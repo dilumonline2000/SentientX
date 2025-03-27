03632bf1b7f698546ff9bec38dfd997ccae35038
Dilum Kariyawasam <165789516+dilumonline2000@users.noreply.github.com>
2025-03-27 17:56:06 +0530

    Setup Landing Page

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ["randomuser.me"], // Use 'domains' instead of 'remotePatterns'
+    },
+  };
+  
+  export default nextConfig;
+  
